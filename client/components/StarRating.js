e945044d5a3517424c19565bcc03fb2e92c69956
finbarrobrien <finbarr.obrien@gmail.com>
2017-09-12 22:43:19 +0100

    forgot what I changed

--- client/components/StarRating.js
+++ client/components/StarRating.js
@@ -1,12 +1,13 @@
 import React from 'react';
+import { Glyphicon } from 'react-bootstrap';
 
 const StarRating = ({ stars }) => {
   const s = [];
   if (stars || stars === 0) {
     for (let i = 1; i <= 5; i += 1) {
       i <= stars ?
-        s.push(<span key={i} className="glyphicon glyphicon-star" />) :
-        s.push(<span key={i} className="glyphicon glyphicon-star-empty" />);
+        s.push(<Glyphicon key={ i } glyph="star" />) :
+        s.push(<Glyphicon key={ i } glyph="star-empty" />);
     }
   }
   return s.length ?
